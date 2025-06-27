22911e235bfa9ab2c254dc0d80abeb3f57bd52e9
Chandures-Priyavindya <145960853+Chandures-Priyavindya@users.noreply.github.com>
2025-06-27 10:59:45 +0530

    user management backend

--- ECMS/Frontend/src/App.tsx
+++ ECMS/Frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Signup from "./components/Sign up/Signup";
 import Alert from "./components/Alerts/Alert";
 import Clustering from "./components/Clustering/Clustering";
 import EnergyTracker from "./components/Energy Tracking/pages/EnergyTracker";
+import AddUser from "./components/User Management/Adduser";
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
         <Route path="/alert" element={<Alert />} />
         <Route path="/clustering" element={<Clustering />} />
         <Route path="/energy-tracker" element={<EnergyTracker />} />
+
+        <Route path="/add-user" element={<AddUser />} />
         
         
       </Routes>
